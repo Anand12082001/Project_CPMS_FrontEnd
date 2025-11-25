33aa68213e7652c7be92377b5005c256be72af77
Anand12082001 <Anandjayakumardae@gmail.com>
2025-11-25 16:33:17 +0530

    Frontend: updated API URL to production backend

--- src/config/backend_url.jsx
+++ src/config/backend_url.jsx
@@ -1,5 +1,6 @@
 // export const BASE_URL = 'http://localhost:4518';
-export const BASE_URL = 'http://localhost:4158';
+export const BASE_URL= 'https://project-cpms-backend.onrender.com'
 
 // export const BASE_URL = 'https://cpms-api.vercel.app';
 // VITE_API_URL='http://localhost:4158'
+                            
