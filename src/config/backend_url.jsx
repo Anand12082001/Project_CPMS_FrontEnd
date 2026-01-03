fb071532e896e6360f35052785f52e80368cdd67
Anand12082001 <160377737+Anand12082001@users.noreply.github.com>
2026-01-03 11:33:48 +0530

    Update backend_url.jsx

--- src/config/backend_url.jsx
+++ src/config/backend_url.jsx
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://localhost:4518';
-export const BASE_URL='https://project-cpms-backend-1.onrender.com'
+export const BASE_URL='https://project-cpms-backend-1.onrender.com'||'http://localhost:4518'
 
 // export const BASE_URL='https://project-cpms-backend-1.onrender.com'
 
