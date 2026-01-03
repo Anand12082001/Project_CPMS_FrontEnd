0c11917cf1a410f70ae58f51bd61fd2a0548baad
Anand12082001 <160377737+Anand12082001@users.noreply.github.com>
2026-01-03 11:25:33 +0530

    Update BASE_URL to production backend URL

--- src/config/backend_url.jsx
+++ src/config/backend_url.jsx
@@ -1,4 +1,6 @@
-export const BASE_URL = 'http://localhost:4518';
+// export const BASE_URL = 'http://localhost:4518';
+export const BASE_URL='https://project-cpms-backend-1.onrender.com'
+
 // export const BASE_URL='https://project-cpms-backend-1.onrender.com'
 
 // VITE_API_URL='http://localhost:4158'
