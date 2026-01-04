116f9f0e3847504f9a723bdf799b99a2d6c9aad5
Anand12082001 <Anandjayakumardae@gmail.com>
2026-01-04 20:54:04 +0530

    Resolve merge conflict

--- src/config/backend_url.jsx
+++ src/config/backend_url.jsx
@@ -1,5 +1,4 @@
-export const BASE_URL = 'http://localhost:4158';
-// export const BASE_URL='https://project-cpms-backend-1.onrender.com'
 
-// VITE_API_URL='http://localhost:4158'
-                            
+// export const BASE_URL='https://project-cpms-backend-1.onrender.com'
+   // export const BASE_URL = 'http://localhost:4518';
+export const BASE_URL = import.meta.env.VITE_API_URL;
